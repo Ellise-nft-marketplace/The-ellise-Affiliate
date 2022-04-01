552f3fe5500208263464f30431c61c71ea85ae38
Aytech00 <Ayokunumitoluwani00@gmail.com>
2022-04-01 03:41:56 -0700

    New push

---
+++ js/el_app.js
@@ -0,0 +1,157 @@
+
+		function mount_data() {
+
+			window.__ELISE_DATA__ = JSON.parse(document.getElementById("__ELISE_DATA__").textContent);
+
+		}
+		function setCookie(cname, cvalue, exhrs) {
+			const d = new Date();
+			d.setTime(d.getTime() + (exhrs * 60 * 60 * 1000));
+			let expires = "expires="+d.toUTCString();
+			document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+		}
+
+		function getCookie(cname) {
+			let name = cname + "=";
+			let ca = document.cookie.split(';');
+			for(let i = 0; i < ca.length; i++) {
+				let c = ca[i];
+				while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+				}
+				if (c.indexOf(name) == 0) {
+					return c.substring(name.length, c.length);
+				}
+			}
+			return "";
+		}
+		function getLoginData() {
+			return {
+				formData: {
+					email: "",
+					password: ""
+				},
+				status: false,
+				loading: false,
+				loggedin: false,
+				isError: false,
+				errorMsg: "",
+				buttonLabel: 'Log in',
+				apiUrl: {
+					baseUrl: '',
+					liveApiHost: 'https://pacific-bayou-81308.herokuapp.com',
+					loginEndpoint: '/api/login',
+				},
+				submitLogin() {
+					// Ensures all fields have data before submitting
+					if (!this.formData.email.length || 
+						!this.formData.password.length) {
+						// alert("Please fill out all required field and try again!")
+						return;
+				}
+					this.buttonLabel = 'Logging in...'
+					this.loading = true;
+					fetch(this.apiUrl.liveApiHost+this.apiUrl.loginEndpoint, {
+							method: 'POST',
+							headers: {
+								'Content-Type': 'application/json',
+								// 'Access-Control-Allow-Origin': '*',
+							},
+							body: JSON.stringify(this.formData)
+						})
+						.then((response) => {
+							if(response.status === 200) {
+								this.status = true;
+								
+								return response.text();
+							} else{
+								throw new Error ("Login failed");
+							}
+
+						})
+						.then((resJson) => {
+							console.log(resJson);
+							if (resJson.access_token && resJson.refresh_token) {
+									setCookie("access_token",resJson.access_token,0.25);
+									setCookie("refresh_token",resJson.refresh_token);
+							}
+							else {
+								this.isError = true;
+								this.errorMsg = resJson;
+							}
+						})
+						.catch((error) => {
+							this.isError = true;
+							console.log(error);
+						})
+						.finally(() => {
+							this.loading = false;
+							this.buttonLabel = 'Log in'
+						})
+				}
+			}
+		}
+		function getRegData() {
+			return {
+				formData: {
+					firstName: "",
+					lastName: "",
+					email: "",
+					password: "",
+					passwordConfirmation: "",
+				},
+				status: false,
+				loading: false,
+				loggedin: false,
+				isError: false,
+				errorMsg: "",
+				buttonLabel: 'Register',
+				apiUrl: {
+					baseUrl: '',
+					liveApiHost: 'https://pacific-bayou-81308.herokuapp.com',
+					regEndpoint: '/api/users',
+				},
+				submitReg() {
+					// Ensures all fields have data before submitting
+					if (!this.formData.email.length || 
+						!this.formData.password.length) {
+						// alert("Please fill out all required field and try again!")
+						return;
+				}
+					this.buttonLabel = 'Signing up...'
+					this.loading = true;
+					fetch(this.apiUrl.liveApiHost+this.apiUrl.regEndpoint, {
+							method: 'POST',
+							headers: {
+								'Content-Type': 'application/json',
+								// 'Access-Control-Allow-Origin': '*',
+							},
+							body: JSON.stringify(this.formData)
+						})
+						.then((response) => {
+							if(response.status === 200) {
+								this.status = true;
+								
+								return response.json();
+							} else{
+								throw new Error ("SignUp failed");
+							}
+
+						})
+						.then((resJson) => {
+							console.log(resJson);
+								this.isError = true;
+								this.errorMsg = resJson.message;
+							
+						})
+						.catch((error) => {
+							this.isError = true;
+							console.log(error);
+						})
+						.finally(() => {
+							this.loading = false;
+							this.buttonLabel = 'Log in'
+						})
+				}
+			}
+		}
